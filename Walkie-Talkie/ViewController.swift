c2bc69ac23647e853fe30aaab6c10a50b9b7a265
Eugene <jekahy@gmail.com>
2017-02-18 15:56:36 +0100

    Initial Commit

---
+++ Walkie-Talkie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Walkie-Talkie
+//
+//  Created by Eugene on 18.02.17.
+//  Copyright © 2017 Eugenious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
